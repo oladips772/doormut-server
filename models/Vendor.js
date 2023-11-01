c81c8b69e9fc861fa34a900a259f3fcaefbc8bca
oladips <oladips200@gmail.com>
2023-11-01 05:50:00 +0100

    working on vendor model

---
+++ models/Vendor.js
@@ -0,0 +1,60 @@
+/** @format */
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const vendorSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isSuspended: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    isMasterVendor: {
+      type: Boolean,
+      default: false,
+    },
+    profileImage: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// login
+vendorSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// register
+vendorSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const Vendor = mongoose.model("Vendor", vendorSchema);
+module.exports = Vendor;
