53d5f09d1765098ad9e4ff84955a469ec955fb0b
oladips <oladips200@gmail.com>
2023-11-01 05:48:22 +0100

    completed admin model

--- models/Admin.js
+++ models/Admin.js
@@ -3,7 +3,40 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const adminSchema = mongoose.Schema(
-  {},
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isSuspended: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    isMasterAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profileImage: {
+      type: String,
+      required: false,
+    },
+  },
   {
     timestamps: true,
   }
@@ -25,4 +58,3 @@ adminSchema.pre("save", async function (next) {
 
 const Admin = mongoose.model("Admin", adminSchema);
 module.exports = Admin;
-
